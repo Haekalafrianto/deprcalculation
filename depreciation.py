4880dfc7e211674f15f0681f4cf4fed390ecbc2d
Haekalafrianto <h.n.afrianto@gmail.com>
2025-05-07 20:15:51 +0700

    added depreciation code

---
+++ depreciation.py
@@ -0,0 +1,29 @@
+def straight_line_method(cost, residue, life):
+    annualdep = (cost - residue)/ life
+    return [round(annualdep)] * life
+
+def double_declining_method(cost, residue, life):
+    rate = 2 / life
+    values = []
+    for year in range(1, life + 1):
+        depreciation = cost * rate
+        if cost - depreciation < residue:
+            depreciation = cost - residue
+        values.append(round(depreciation, 2))
+        cost -= depreciation
+    return(values)
+
+def sum_of_the_year_method(cost, residue, life):
+    total = life * (life + 1) / 2
+    values = []
+    for year in range(life):
+        factor = (life - year) / total
+        depreciation = (cost - residue) * factor
+        values.append(round(depreciation, 2))
+    return values
+
+cost = 1000
+residue = 100
+life = 5
+
+print(sum_of_the_year_method(cost, residue, life))
