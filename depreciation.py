70ba1be4c6a6fd5d4e71f69200dfab02c06f4a75
haekalafrianto <h.n.afrianto@gmail.com>
2025-05-14 14:45:49 +0700

    added console

--- depreciation.py
+++ depreciation.py
@@ -1,3 +1,5 @@
+# main function
+
 def straight_line_method(cost, residue, life):
     annualdep = (cost - residue)/ life
     return [round(annualdep)] * life
@@ -21,3 +23,35 @@ def sum_of_the_year_method(cost, residue, life):
         depreciation = (cost - residue) * factor
         values.append(round(depreciation, 2))
     return values
+
+# console
+
+
+print("Welcome to Depreciation Calculation")
+print("There list of depreciation method \n 1. Straight Line Method \n 2. Double Declining Method \n 3. Sum of the Year")
+
+while True:
+
+    depreciaton = int(input("Please input depreciation you want (1/2/3) "))
+    cost = float(input("Please input price of the goods "))
+    residue = float(input("Please input the residue of the goods "))
+    life = int(input("Please input the ideal life of the goods "))
+
+    if depreciaton == 1:
+        print(straight_line_method(cost, residue, life))
+    elif depreciaton == 2:
+        print(double_declining_method(cost, residue, life))
+    elif depreciaton == 3:
+        print(sum_of_the_year_method(cost, residue, life))
+    else:
+        print("Invalid input, can't calculate")
+
+    again = input("Do you want calculate again? (y/n) ")
+    if again == "y":
+        continue
+    elif again == "n":
+        print("exiting the programs...")
+        break
+    else:
+        print("invalid input. exiting by default...")
+        break 
